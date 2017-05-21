7616db1810c3d033d048bf0bb12ab0291394ac72
Harshdeep Singh <harsh.batth@gmail.com>
2017-05-21 13:02:30 +0530

    Completed setting up firebase

---
+++ Pods/Target Support Files/Pods-facebook-social/Pods-facebook-social-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_facebook_social : NSObject
+@end
+@implementation PodsDummy_Pods_facebook_social
+@end
---
+++ facebook-social/SignInVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  facebook-social
+//
+//  Created by Harshdeep  Singh on 21/05/17.
+//  Copyright © 2017 Harshdeep  Singh. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
